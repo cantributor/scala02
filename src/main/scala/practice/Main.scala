a6a4d2b17ef1b58e0773bb29046b163c23ad257e
can <alexeychub@gmail.com>
2025-05-07 17:06:45 +0300

    simple thread example

--- src/main/scala/practice/Main.scala
+++ src/main/scala/practice/Main.scala
@@ -1,5 +1,18 @@
-package practice;
+package practice
 
 object Main {
-  def main(args: Array[String]): Unit = println("Hello world!")
+  def main(args: Array[String]): Unit = {
+    class CustomThread extends Thread {
+      override def run(): Unit = {
+        println("custom thread running...")
+        Thread.sleep(3000)
+      }
+    }
+
+    val thread = new CustomThread
+    thread.start()
+    thread.join()
+
+    println("custom thread has completed its execution")
+  }
 }
