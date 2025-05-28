5a9cc59993401d459f92d0bc8cce9eb7e3b64ab4
can <alexeychub@gmail.com>
2025-05-28 18:20:44 +0300

    part 2 parallels fix

--- src/main/scala/practice/p2parallels/Parallels.scala
+++ src/main/scala/practice/p2parallels/Parallels.scala
@@ -3,5 +3,5 @@ package practice.p2parallels
 import scala.collection.parallel.CollectionConverters.seqIsParallelizable
 
 object Parallels {
-  def countNonEmpty(sq: Seq[String]): Int = sq.par.par.count(s => s.nonEmpty)
+  def countNonEmpty(sq: Seq[String]): Int = sq.par.count(s => s.nonEmpty)
 }
